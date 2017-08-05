f73120ff5b00ffe11d5bc4a9292519f5816f6d9a
Xenos Konstantinos <mr.xkon@gmail.com>
2017-08-05 10:19:40 +0300

    editor style
    editor style

--- simplespace/functions.php
+++ simplespace/functions.php
@@ -26,6 +26,12 @@ function simplespace_setup() {
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'custom-logo' );
+	add_editor_style(
+		array(
+			'bootstrap/css/bootstrap.css',
+			'css/sp-editor.css',
+		)
+	);
 }
 add_action( 'after_setup_theme', 'simplespace_setup' );
 
