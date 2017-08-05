f871c0dab5c040c004c0183af560386051bdf01e
Xenos Konstantinos <mr.xkon@gmail.com>
2017-08-05 10:31:17 +0300

    custom background support
    custom background support

--- simplespace/functions.php
+++ simplespace/functions.php
@@ -26,6 +26,7 @@ function simplespace_setup() {
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'custom-logo' );
+	add_theme_support( 'custom-background' );
 	add_editor_style(
 		array(
 			'bootstrap/css/bootstrap.min.css',
